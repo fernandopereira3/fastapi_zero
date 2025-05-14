7389cf55e7250c9ebe76041c30ad53d741d1bc01
Fernando Pereira <fernandopereira-3@hotmail.com>
2025-05-14 00:10:05 -0300

    Adicionado Schemas

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,35 @@
+from http import HTTPStatus
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse, RedirectResponse
+
+from fastapi_zero.schemas import message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', 
+        status_code=HTTPStatus.OK,
+        response_model=message,
+        )
 def read_root():
-    return {'mensage': 'Hello Word!'}
+    return {'message': 'Hello World!'}
+
+@app.get('/html', response_class=HTMLResponse)
+def exercicio_aula_02():
+    return """
+    <html>
+      <head>
+        <title>Hello World!</title>
+      </head>
+      <body>
+        <h1> Hello World! </h1>
+      </body>
+    </html>"""
+
+@app.get('/exercicio2', response_class=HTMLResponse)
+def exercicio2():
+    return """<h1>ESTOU VIVO</h1>"""
+
+@app.get("/duno")
+async def redirect_typer():
+    return RedirectResponse("https://fastapidozero.dunossauro.com/estavel/02/")
